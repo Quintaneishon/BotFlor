c02466e208e2d967019d2cefc51e46b86f68092f
EduardoRodriguezRicardez <xedrodrr@montepiedad.com.mx>
2021-02-25 10:09:03 -0600

    add safepooling

--- main.py
+++ main.py
@@ -3,21 +3,9 @@ import datetime
 import Operations as op
 from Alumno import Alumno
 
-API_TOKEN = '1577711933:AAG6LHZB6ryjZne5krMIYIe3Rt4w4r9GBmE'
+API_TOKEN = ''
+owner = 0
 bot = telebot.TeleBot(API_TOKEN)
-print("""
-                    ___                         ___                         ___           ___     
-     _____         /\  \                       /\__\                       /\  \         /\  \    
-    /::\  \       /::\  \         ___         /:/ _/_                     /::\  \       /::\  \   
-   /:/\:\  \     /:/\:\  \       /\__\       /:/ /\__\                   /:/\:\  \     /:/\:\__\  
-  /:/ /::\__\   /:/  \:\  \     /:/  /      /:/ /:/  /    ___     ___   /:/  \:\  \   /:/ /:/  /  
- /:/_/:/\:|__| /:/__/ \:\__\   /:/__/      /:/_/:/  /    /\  \   /\__\ /:/__/ \:\__\ /:/_/:/__/___
- \:\/:/ /:/  / \:\  \ /:/  /  /::\  \      \:\/:/  /     \:\  \ /:/  / \:\  \ /:/  / \:\/:::::/  /
-  \::/_/:/  /   \:\  /:/  /  /:/\:\  \      \::/__/       \:\  /:/  /   \:\  /:/  /   \::/~~/~~~~ 
-   \:\/:/  /     \:\/:/  /   \/__\:\  \      \:\  \        \:\/:/  /     \:\/:/  /     \:\~~\     
-    \::/  /       \::/  /         \:\__\      \:\__\        \::/  /       \::/  /       \:\__\    
-     \/__/         \/__/           \/__/       \/__/         \/__/         \/__/         \/__/    
-""")
 
 # MENSAJE BIENVEIDA
 @bot.message_handler(commands=['start'])
@@ -62,4 +50,60 @@ def echo_message(message):
                 bot.send_message(message.chat.id, f"___{alumno.nombre} - {materia.upper()}___ [👨🏼‍🎓]\n\nTienes {alumno.promedio} [📝]\n\nDado tu puntaje eres el numero {alumno.ranking} [🌟]",parse_mode="MARKDOWN")
                 with open('log.txt', 'a', encoding="UTF-8") as f:
                     f.write(f"{message.from_user.username} | {alumno.nombre} | {materia} | {datetime.datetime.now()}\n")
-bot.polling()
+def safepolling(bot):
+    if(bot.skip_pending):
+        lid = bot.get_updates()[-1].update_id
+    else:
+        lid = 0
+    while(1):
+        try:
+            updates = bot.get_updates(lid + 1, 50)
+            # print('len updates = %s' % len(updates))
+            if(len(updates) > 0):
+                lid = updates[-1].update_id
+                bot.process_new_updates(updates)
+        except telebot.ApiException as a:
+            print(a)
+        except Exception as e:
+            print('Exception at %s \n%s' % (asctime(), e))
+            now = int(time())
+            while(1):
+                error_text = 'Exception at %s:\n%s' % (asctime(), str(e) if len(str(e)) < 3600 else str(e)[:3600])
+                try:
+                    # print('Trying to send message to owner.')
+                    offline = int(time()) - now
+                    bot.send_message(owner, error_text + '\nBot went offline for %s seconds' % offline)
+                    # print('Message sent, returning to polling.')
+                    break
+                except:
+                    sleep(0.25)
+if(__name__ == '__main__'):
+    # Bot starts here.
+    print('Bot started.')
+    try:
+        print('Bot username:[%s]' % bot.get_me().username)
+    except telebot.ApiException:
+        print('The given token [%s] is invalid, please fix it')
+        exit(1)
+    # Tell owner the bot has started.
+    try:
+        bot.send_message(owner, 'Bot Started')
+    except telebot.ApiException:
+        print('''Make sure you have started your bot https://telegram.me/%s.
+    And configured the owner variable.''' % bot.get_me().username)
+        exit(1)
+    print("""
+                    ___                         ___                         ___           ___     
+     _____         /\  \                       /\__\                       /\  \         /\  \    
+    /::\  \       /::\  \         ___         /:/ _/_                     /::\  \       /::\  \   
+   /:/\:\  \     /:/\:\  \       /\__\       /:/ /\__\                   /:/\:\  \     /:/\:\__\  
+  /:/ /::\__\   /:/  \:\  \     /:/  /      /:/ /:/  /    ___     ___   /:/  \:\  \   /:/ /:/  /  
+ /:/_/:/\:|__| /:/__/ \:\__\   /:/__/      /:/_/:/  /    /\  \   /\__\ /:/__/ \:\__\ /:/_/:/__/___
+ \:\/:/ /:/  / \:\  \ /:/  /  /::\  \      \:\/:/  /     \:\  \ /:/  / \:\  \ /:/  / \:\/:::::/  /
+  \::/_/:/  /   \:\  /:/  /  /:/\:\  \      \::/__/       \:\  /:/  /   \:\  /:/  /   \::/~~/~~~~ 
+   \:\/:/  /     \:\/:/  /   \/__\:\  \      \:\  \        \:\/:/  /     \:\/:/  /     \:\~~\     
+    \::/  /       \::/  /         \:\__\      \:\__\        \::/  /       \::/  /       \:\__\    
+     \/__/         \/__/           \/__/       \/__/         \/__/         \/__/         \/__/    
+""")
+    print('Safepolling Start.')
+    safepolling(bot)
